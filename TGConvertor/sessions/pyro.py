2922ea836b9a7a5571b80b6e49c588b4a2f3d904
Snek <97262062+sn3kdev@users.noreply.github.com>
2024-12-25 16:09:03 +0200

    Update for new versions of Pyrogram to work

--- TGConvertor/sessions/pyro.py
+++ TGConvertor/sessions/pyro.py
@@ -8,11 +8,12 @@ import aiosqlite
 from opentele.api import APIData
 from pyrogram.client import Client
 
-from ..exceptions import ValidationError
+from rgt_auth.core.convertor.exceptions import ValidationError
 
 
 SCHEMA = """
-CREATE TABLE sessions (
+CREATE TABLE sessions
+(
     dc_id     INTEGER PRIMARY KEY,
     api_id    INTEGER,
     test_mode INTEGER,
@@ -22,22 +23,40 @@ CREATE TABLE sessions (
     is_bot    INTEGER
 );
 
-CREATE TABLE peers (
+CREATE TABLE peers
+(
     id             INTEGER PRIMARY KEY,
     access_hash    INTEGER,
     type           INTEGER NOT NULL,
-    username       TEXT,
     phone_number   TEXT,
     last_update_on INTEGER NOT NULL DEFAULT (CAST(STRFTIME('%s', 'now') AS INTEGER))
 );
 
-CREATE TABLE version (
+CREATE TABLE usernames
+(
+    id       INTEGER,
+    username TEXT,
+    FOREIGN KEY (id) REFERENCES peers(id)
+);
+
+CREATE TABLE update_state
+(
+    id   INTEGER PRIMARY KEY,
+    pts  INTEGER,
+    qts  INTEGER,
+    date INTEGER,
+    seq  INTEGER
+);
+
+CREATE TABLE version
+(
     number INTEGER PRIMARY KEY
 );
 
 CREATE INDEX idx_peers_id ON peers (id);
-CREATE INDEX idx_peers_username ON peers (username);
 CREATE INDEX idx_peers_phone_number ON peers (phone_number);
+CREATE INDEX idx_usernames_id ON usernames (id);
+CREATE INDEX idx_usernames_username ON usernames (username);
 
 CREATE TRIGGER trg_peers_last_update_on
     AFTER UPDATE
@@ -196,5 +215,8 @@ class PyroSession:
                 self.user_id or 9999,
                 self.is_bot
             )
+            version_sql = "INSERT INTO version VALUES (6)"
             await db.execute(sql, params)
+            await db.execute(version_sql)
             await db.commit()
+        return path
